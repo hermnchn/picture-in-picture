de57980d4e3b0ac550ec7700f63806f57ce44dec
Herman <hermnchn@gmail.com>
2022-04-01 00:20:45 -0700

    Update

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const button = document.getElementById('button');
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
     try {
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-        videoElement.scrObject = mediaStream;
-        videoElement.onloadedmetadata = () => {
-            videoElement.play();
-        }
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+      videoElement.srcObject = mediaStream;
+      videoElement.onloadedmetadata = () => {
+        videoElement.play();
+      };
     } catch (error) {
         // Catch Error Here
         console.log('whoops, error here:', error);
@@ -22,7 +22,8 @@ button.addEventListener('click', async () => {
     await videoElement.requestPictureInPicture();
     // Reset Button
     button.disabled = false;
-});
-
-// On Load
-selectMediaStream();
+  });
+  
+  // On Load
+  selectMediaStream();
+  
