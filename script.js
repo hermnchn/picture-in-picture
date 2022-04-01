c5e43fc4b5a866ede4c0a1aa54b76675fe63021d
Herman <hermnchn@gmail.com>
2022-03-31 23:41:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.scrObject = mediaStream;
+        
+    } catch (error) {
+        // Catch Error Here
+        console.log('whoops, error here:', error);
+    }
+}
